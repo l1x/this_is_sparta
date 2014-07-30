5544ced20851c3c717718e17d59a104ecfb418c3
Istvan Szukacs <leccine@gmail.com>
2014-07-30 06:43:46 -0700

    Adding first spike

--- this_is_sparta.erl
+++ this_is_sparta.erl
@@ -17,129 +17,39 @@
 -author('leccine@gmail.com').
 
 -export([
-  delete_all_buckets/0, 
-  delete_all_buckets/1
+  delete_all_buckets/0 
 ]).
 
 -define(CLUSTERS, [
-  {cluster0,["node0", "node1", "node2"]},
-  {cluster1,["node0", "node1", "node2"]}
+  {cluster0,["node0:port0", "node1:port1", "node2:port2"]},
+  {cluster1,["node0:port0", "node1:port1", "node2:port2"]}
 ]).
 
 delete_all_buckets() ->
-  error_logger:error_msg("Please specify a file ~n", []),
-  {error, missing_file}.
+  %% connects to Riak cluster 
+  %% queries the to_be_deleted bucket for keys using 2i
+  %% if finds keys checks if they exist in has_been_deleted
+  %% if not deletes all of its keys and inserts it to the has_been_deleted
+  %% TODO: open multiple connection, run parallel in different clusters
 
-delete_all_buckets(Bucket) ->
-  {ok, ok}.
+  %{ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 8087).
+  %{ok, O} = riakc_pb_socket:get(Pid, <<"groceries">>, <<"mine">>).
+  %riakc_pb_socket:get_bucket(Pid, <<"groceries">>).
 
-delete_all_buckets_loop(File) ->
-  receive
-    {Pid, started, {perf, 0}} -> 
-      %io:format("Got: ~p~n", [{Pid, started}]),
-      ets:insert(process_registry, {Pid, started, {perf, 0}}),
-      delete_all_buckets_loop(File);
-    {Pid, running, {perf, Perf}} ->
-      ets:insert(process_registry, {Pid, started, {perf, Perf}}),
-      delete_all_buckets_loop(File);
-    {'EXIT',Pid,_} ->
-      %io:format("Got: ~p~n", [{'EXIT', Pid}]),
-      ets:delete(process_registry, Pid),
-      Len = length(ets:tab2list(process_registry)),
-      case Len of
-        %% save .done file
-        0 ->
-          file:write_file( string:concat(atom_to_list(File), ".done") , "ok"),
-          init:stop();
-        _ -> 
-          delete_all_buckets_loop(File)
-      end;
-    alarm ->
-      Len = length(ets:tab2list(process_registry)),
-      io:format("The current performance is: ~p req/s Running processes: ~p~n", [
-        lists:sum(lists:flatten(ets:select(process_registry,[{{'_','_',{'_','$1'}},[],['$$']}]))), Len
-      ]),
-      erlang:garbage_collect(self()),
-      set_alarm(2000),
-      delete_all_buckets_loop(File);
-    Else ->
-      io:format("Got: ~p~n", [Else])
-  after
-    1000 -> 
-      %%io:format("Timeout: ~p~n", [timeout]),
-      delete_all_buckets_loop(File)
-  end.
-
-delete_a_bucket(Bucket) ->
-  io:format("Deleting: ~p~n", [Bucket]),
-  Pid = self(),
+  %% creating a connection registry
+  ets:new(connection_registry, [bag, named_table]),
   lists:foreach(fun(Node) ->
-    spawn_link(fun() -> delete_a_bucket(Bucket, Node, Pid) end)
-  end, [Node || {_,List} <- ?CLUSTERS, Node <- List]),
-  ok.
+    [Host, Port] = string:tokens(Node,":"),
+    io:format("Connecting to host: ~p on port: ~p ~n", [Host, Port]),
+    case riakc_pb_socket:start_link(Host, Port) of
+      {ok, Pid} -> 
+        error_logger:info_msg("Successfully connected to ~p~n", [Host]),
+        ets:insert(connection_registry,{Host, Pid});
+      _ -> 
+        error_logger:error_msg("Cannot connect to ~p", [Host])
+    end
+  end, [Node || {_,List} <- ?CLUSTERS, Node <- List]).
 
-delete_a_bucket(Bucket, Node, ParentPid) ->
-  io:format("Starting: ~p Bucket:~p Node:~p ParPid:~p ~n", [self(), Bucket, Node, ParentPid]),
-  ParentPid ! {self(), started, {perf, 0}},
-  {ok, Pid} = riakc_pb_socket:start_link(Node, 8087),
-  riakc_pb_socket:stream_list_keys(Pid, Bucket),
-  delete_loop(Pid, Bucket, ParentPid).
 
-delete_loop(Pid, Bucket, ParentPid) ->
-  erlang:garbage_collect(self()),
-  receive
-    {_, {_, List}} ->
-      %%This is happening inside a process
-      %%so doing synchronous sequential deletes are ok
-      %%if not use pmap instead of foreach with callbacks 
-      %%start timer
-      Len = length(List),
-      {Time,_} = timer:tc(fun() ->
-        lists:foreach(fun(K) -> 
-          riakc_pb_socket:delete(Pid, Bucket, K) 
-        end, List) 
-      end),
-      ReqPerSec = req_per_sec(Time, Len),
-      ParentPid ! {self(), running, {perf, ReqPerSec}},
-      delete_loop(Pid, Bucket, ParentPid);
-    {_, done} ->
-      io:format("Finished with all of the keys in the ~p bucket.~nExiting process: ~p~n", [Bucket, self()]),
-      exit("Bucket is done, terminating worker...");
-    Else ->
-      io:format("Got something else: ~p, terminating~n", [Else])
-  end.
 
-req_per_sec(_Time, 0) ->
-  0;
-req_per_sec(Time, Len) -> 
-  trunc((Time / 1000) / Len).
 
-set_alarm(T) ->
-  Pid = self(),
-  spawn_link(fun() -> set(Pid, T) end).
-
-set(Pid, T) ->
-  receive
-  after T -> Pid ! alarm
-  end.
-
-randomize_list(List) ->
-  {ok, [X||{_,X} <- lists:sort([ {random:uniform(), N} || N <- List]) ]}.
-
-file_to_list(File) ->
-  case filelib:is_file(File) of
-    %%if it is a file
-    true ->
-      case file:consult(File) of
-        {ok, List} ->
-          %%error_logger:info_msg("Bucket list is read from file: ~p~n", [File]),
-          {ok, List};
-        {error,Reason} ->
-          error_logger:error_msg("Cannot open file: ~p Reason: ~p~n", [File, Reason]),
-          {error,Reason}
-      end;
-    %if it is NOT a file
-    false ->
-      error_logger:error_msg("Not a file: ~p~n", [File]),
-      {error, not_a_file}
-  end.
